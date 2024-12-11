e1b4e8eae9ea1d41dd7beacfc75dd3fa10077c99
Clorinda NGUOKO <clorindalychelle@gmail.com>
2024-12-11 15:15:55 +0100

    ohce test written

--- java/src/test/java/info/dmerej/OhceTest.java
+++ java/src/test/java/info/dmerej/OhceTest.java
@@ -3,8 +3,26 @@ package info.dmerej;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class OhceTest {
+  private Ohce ohce;
+  @Mock
+  private ConsoleInteractor mockInteractor;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+    ohce = new Ohce(mockInteractor);
+  }
   @Test
   void testMainLoop() {
     /*
@@ -18,7 +36,21 @@ public class OhceTest {
      - oto
      - That was a palindrome!
 
-    */
-    fail("TODO");
+    fail("TODO");*/
+
+    // Arrange
+    when(mockInteractor.readInput())
+            .thenReturn("hello")
+            .thenReturn("oto")
+            .thenReturn("quit");
+
+    // Act
+    ohce.mainLoop();
+
+    // Assert
+    InOrder inOrder = inOrder(mockInteractor);
+    ((InOrder) inOrder).verify(mockInteractor).printMessage("olleh");
+    inOrder.verify(mockInteractor).printMessage("oto");
+    inOrder.verify(mockInteractor).printMessage("That was a palindrome!");
   }
 }
